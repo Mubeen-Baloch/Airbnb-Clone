6ed284da13804a723c7857f30f28093ed2ecfb43
Mubeen-Baloch <blouchmubeen@gmail.com>
2025-08-10 12:32:05 +0500

    Fix Google OAuth callback URL to resolve Chrome security warning

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000',
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/api/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ email: profile.emails[0].value });
